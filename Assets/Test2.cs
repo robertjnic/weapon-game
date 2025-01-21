f0efa5d25c986095d8c88ae58a41f2ab1dbc7a4a
robert <robertjnic@gmail.com>
2025-01-20 20:11:51 -0700

    change from 3d to 2d

--- Assets/Test2.cs
+++ Assets/Test2.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Test2 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
